07797b89700403cbd2c2ed742bf843f56bbecdb3
Shah-Faisal-cloud <fjcreater99@gmail.com>
2025-09-17 17:05:01 +0500

    Updated

--- src/script.js
+++ src/script.js
@@ -9,36 +9,40 @@ function getTimeFrame(e) {
 function updateCards(data, activeTimePeriod = 'daily') {
     data.forEach((item, index) => {
         const card = cards[index];
-        
+
         const title = card.querySelector(".card-title");
         const currentHrs = card.querySelector("[data-current-hours]");
         const prevHrs = card.querySelector("[data-prev-hours]");
-        
+
         title.textContent = item.title;
         currentHrs.textContent = item.timeframes[activeTimePeriod].current;
         prevHrs.textContent = item.timeframes[activeTimePeriod].previous;
-        
+
     });
 };
 
 async function startApp() {
-    const response = await fetch("./src/data.json");
-    const data = await response.json();
-    
-    buttons[0].classList.add("active");
-    
-    buttons.forEach(button => {
-        button.addEventListener("click", (e) => {
+    try {
+        const response = await fetch("./src/data.json");
+        const data = await response.json();
 
-            buttons.forEach(button => button.classList.remove("active"));
-            e.currentTarget.classList.add("active");
-            
-            const selectedPeriod = getTimeFrame(e);
-            updateCards(data, selectedPeriod);
+        buttons[0].classList.add("active");
+
+        buttons.forEach(button => {
+            button.addEventListener("click", (e) => {
+
+                buttons.forEach(button => button.classList.remove("active"));
+                e.currentTarget.classList.add("active");
+
+                const selectedPeriod = getTimeFrame(e);
+                updateCards(data, selectedPeriod);
+            });
         });
-    });
 
-    updateCards(data);
+        updateCards(data);
+    } catch (error) {
+        console.log("Error loading data: ", error);
+    }
 };
 
 startApp();
